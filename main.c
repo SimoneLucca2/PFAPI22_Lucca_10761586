5d8bb480a0f52753dde2eba3b270d61bc5ed71f4
SimoneLucca2 <106387524+SimoneLucca2@users.noreply.github.com>
2022-06-24 18:13:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,47 @@
+#include <stdlib.h>
 #include <stdio.h>
+
+int n, k;
+
+void read_legal_words(){
+    char *word;
+    while(1){
+        if(scanf("%s", word) != 1) return;
+        if(word[0] == '+' && word[1] == 'n') break; //when the user types +nuova_partita it stops reading
+        //INSERT word IN DATA STRUCTURE
+    }
+}
+
+
+int main() {
+    //reading and storing part
+        //read k
+        //read words
+        //store each word
+    if(scanf("%d", &k) != 1) return 0;
+    read_legal_words();
+
+
+    //game part
+        //read reference word
+        //read n
+        //read word p - note exceptions +inserisci_inizio
+        //check if p respects constraints
+        //print res  / ok / ko
+        //apply constrains
+        //count every legal word remaining
+
+    char *r;
+    if(scanf("%s", r) != 1) return 0;
+    if(scanf("%d", &n) != 1) return 0;
+
+    while(1){
+        if(scanf("%s", p) != 1) return 0;
+        if(p[0] == '+' && p[1] == 'i') //CALL READ FUNCTION
+    }
+
+
+
+
+}
+
